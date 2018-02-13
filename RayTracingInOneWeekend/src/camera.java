43dc7b51192058a521491b76269f00319601f88f
Skrillcon <guardianx515@gmail.com>
2018-02-13 09:29:12 -0600

    Can get normal maps for spheres

--- RayTracingInOneWeekend/src/camera.java
+++ RayTracingInOneWeekend/src/camera.java
@@ -30,7 +30,7 @@ public class camera {
 		}
 		
        try {  
-            Writer w = new FileWriter("FlatSphere.ppm");  
+            Writer w = new FileWriter("NormalSphere.ppm");  
             w.append(sb);  
             w.close();  
             System.out.println("Done");  
@@ -39,7 +39,8 @@ public class camera {
         }  
 	}
 	
-	private static boolean hit_sphere(Vec3 center, float radius, ray r) {
+	
+	private static float hit_sphere(Vec3 center, float radius, ray r) {
 		// r.origin - center
 		Vec3 oc = r.origin().sub(center);
 		
@@ -48,15 +49,25 @@ public class camera {
 		float c = oc.dot(oc) - radius * radius;
 		float discriminant = b * b - 4 * a * c;
 		
-		return (discriminant > 0);
+		if (discriminant < 0) {
+			return -1.0f;
+		} else {
+			// return normal: direction of hitpoint minus center
+			return (float) ((-b - Math.sqrt(discriminant))/(2.0f * a));
+		}
 	}
 	
 	private static Vec3 color(ray r) {
-		if(hit_sphere(new Vec3(0f, 0f, -1f), 0.5f, r))
-			return new Vec3(1f, 0f, 0f);
+		float t = hit_sphere(new Vec3(0f, 0f, -1f), 0.5f, r);
+		if(t > 0.0f){
+			Vec3 N = r.point_at_parameter(t).sub(new Vec3(0f, 0f, -1f)).normalize();
+			// Change the -1 to 1 coordinates to 0 to 1
+			// 0.5f * new Vec3(N.x() + 1f, N.y() + 1f, N.z() + 1f)
+			return N.add(1f).mul(.5f);
+		}
 		
-		Vec3 unit_direction = Vec3.unit_vector(r.direction());
-		float t = (float) (0.5f * (unit_direction.y() + 1.0));
+		Vec3 unit_direction = r.direction().normalize();
+		t = (float) (0.5f * (unit_direction.y() + 1.0));
 		Vec3 white = new Vec3(1.0f, 1.0f, 1.0f);
 		Vec3 blue = new Vec3(0.5f, 0.7f, 1.0f);
 		return white.mul(1.0f - t).add(blue.mul(t));
