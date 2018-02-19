0be30ef3d615ace170f5a36ab6d27f127ee87f1c
Skrillcon <guardianx515@gmail.com>
2018-02-19 09:27:04 -0600

    Buggy code, pushing to test on desktop.

--- RayTracer/RayTracer/main.cpp
+++ RayTracer/RayTracer/main.cpp
@@ -9,22 +9,36 @@ bool hitSphere(const vec& center, float radius, const ray& r) {
 	float a = dot(r.direction(), r.direction());
 	float b = 2.0 * dot(oc, r.direction());
 	float c = dot(oc, oc) - radius * radius;
-	float discriminant = b * b - 4 * a * c;
-	return (discriminant > 0);
+	float discriminant = b * b - 4 * a * c; 
+	//return (discriminant > 0);
+	//cout << a << b << c << endl;
+	if (discriminant < 0) {
+		return -1.0;
+	}
+	else {
+		return (-b - sqrt(discriminant) ) / (2.0 * a);
+	}
 }
 
 vec color(const ray& r) {
-	if (hitSphere(vec(0, 0, -1), 0.5, r))
-		return vec(1, 0, 0);
+	float t = hitSphere(vec(0, 0, -1), 0.5, r);
+	if (t) {
+		//return vec(1, 0, 0);
+	}
+	if (t > 0.0) {
+		vec normal = unitVector(r.pointAtParameter(t) - vec(0, 0, -1));
+		return 0.5 * vec(normal.x() + 1, normal.y() + 1, normal.z() + 1);
+	}
+	//cout << " t < 0 " << endl;
 
 	vec unitDirection = unitVector(r.direction());
-	float t = 0.5 * (unitDirection.y() + 1.0);
-	return (1.0 - t) * vec(1.0, 1.0, 1.0) + t * vec(0.5, 0.7, 1.0);
+	t = 0.5 * (unitDirection.y() + 1.0);
+	return ((1.0 - t) * vec(1.0, 1.0, 1.0) + t * vec(0.5, 0.7, 1.0));
 }
 
 int main() {
 	ofstream imgOut;
-	imgOut.open("Test.ppm");
+	imgOut.open("Normal.ppm");
 	int width = 200;
 	int height = 100;
 
